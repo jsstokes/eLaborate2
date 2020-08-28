555c0d9a2380738f1aa210a224b9ad909ae3e219
Scott Stokes <sstokes99@gmail.com>
2020-08-28 07:42:01 -0500

    fixed login issue

--- src/pages/login-page/login-page.page.jsx
+++ src/pages/login-page/login-page.page.jsx
@@ -28,7 +28,7 @@ class LoginPage extends React.Component {
 
     handleSaveButton = (parms) => {
         console.log("Handle Save:", parms);
-        this.context.setAuthorized(false, parms.userid,"");
+        this.context.setAuthorized(true, parms.userid,"");
         // this.props.history.push(`/student/5f35b6b724f7a22dde49e082`); ///${parms.userid}`);
         this.props.history.push(`/`); ///${parms.userid}`);
         return(
